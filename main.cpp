02463c7e9293e01f08c258d54cb6b027321f5130
Karina Rozhkova <kerryrooooo@gmail.com>
2023-07-10 13:11:05 +0300

    code: изменен код

--- main.cpp
+++ main.cpp
@@ -4,16 +4,21 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
-    int max;
+    int max, min;
     cin >> a >> b;
     if(a>b)
         max=a;
     else
         max=b;
+    if(a>b)
+        min=b;
+    else
+        min=a;
     cout << "A + B = "<< a + b <<'\n'
     << "A - B = "<< a - b <<'\n'
     << "A * B = "<< a * b <<'\n'
     << "A / B = "<< a / b <<'\n'
-    << "Max = "<< max <<'\n';
+    << "Max = "<< max <<'\n'
+    << "Min = "<< min <<'\n';
     return 0;
 }
