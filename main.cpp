fd3074ab11296987ce422c1861d7d2c29ec7597c
Bob (RozhkovaKA) <bobby@gmail.com>
2023-07-10 10:34:59 +0300

    code: изменен код

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
     int a, b;
     cin >> a >> b;
     cout << "A + B = "<< a + b <<'\n'
-    << "A - B = "<< a - b <<'\n';
+    << "A - B = "<< a - b <<'\n'
+    << "A * B = "<< a * b <<'\n';
     return 0;
 }
