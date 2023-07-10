ce2f9c15f103e9d2e36e96380b889670d1100767
Alice (RozhkovaKA) <alice2010@gmail.com>
2023-07-10 10:57:58 +0300

    code: изменен код

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
-    cin >> a >> b;
+    int max;
+    cin >> a >> b>> max;
+    if(a>b)
+        max=a;
+    else
+        max=b;
     cout << "A + B = "<< a + b <<'\n'
     << "A - B = "<< a - b <<'\n'
     << "A * B = "<< a * b <<'\n'
-    << "A / B = "<< a / b <<'\n';
+    << "A / B = "<< a / b <<'\n'
+    << "Max = "<< max <<'\n';
     return 0;
 }
