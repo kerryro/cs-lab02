d28e5d5110e8ce1443e57068cde52007122fe413
Alice (RozhkovaKA) <alice2010@gmail.com>
2023-07-10 10:09:28 +0300

    code: дополнен код

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    cout << "A + B = "<< a + b <<'\n'
+    << "A - B = "<< a - b <<'\n';
     return 0;
 }
