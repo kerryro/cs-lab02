c833f89151aaee72bed1dfdc5b129c3a88c82099
Alice (RozhkovaKA) <alice2010@gmail.com>
2023-07-10 15:03:14 +0300

    code: изменен код

--- main.cpp
+++ main.cpp
@@ -3,13 +3,10 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-<<<<<<< HEAD
     double a, b;
     int max;
-=======
     int a, b;
     int max, min;
->>>>>>> 02463c7e9293e01f08c258d54cb6b027321f5130
     cin >> a >> b;
     if(a>b)
         max=a;
